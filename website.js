89ae4657c6b5647a9ef5aea89b612e7c7967cab7
Asad ali <150065701+Asadlakho@users.noreply.github.com>
2025-06-01 07:22:32 -0700

    Update website.js

--- website.js
+++ website.js
@@ -7,3 +7,20 @@ function closeModal() {
   document.getElementById("modal").classList.add("hidden");
   document.getElementById("modal").classList.remove("flex");
 }
+
+ const toggleBtn = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const menuIcon = document.getElementById('menu-icon');
+
+  toggleBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+
+    // Toggle between hamburger and close icon
+    if (menuIcon.classList.contains('fa-bars')) {
+      menuIcon.classList.remove('fa-bars');
+      menuIcon.classList.add('fa-xmark');
+    } else {
+      menuIcon.classList.remove('fa-xmark');
+      menuIcon.classList.add('fa-bars');
+    }
+  });
